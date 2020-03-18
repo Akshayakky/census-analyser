b417d84176679f943bacd9bb364b7e0b0cd8c3da
akshayakky <akshaysmpt01@gmail.com>
2020-03-18 20:39:24 +0530

    ADDED : Test Case TC1.1

--- src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTestCases.java
+++ src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTestCases.java
@@ -1,8 +1,21 @@
 package com.bridgelabz.censusanalyser;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StateCensusAnalyserTestCases {
     private static final String SAMPLE_CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
     private static final String SAMPLE_CSV_INCORRECT_FILE_PATH = "src/test/resources/StateCensusInvalidData.csv";
     private static final String SAMPLE_CSV_INCORRECT_FILE_NAME = "src/test/resources/IncorrectName.csv";
     private static final String SAMPLE_CSV_INCORRECT_FILE_TYPE = "src/test/resources/StateCensusData.pdf";
+
+    @Test
+    public void givenCSVFile_WhenFileCorrect_ThenReturnTrue() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            int counter = stateCensusAnalyser.getNumberOfRecords(SAMPLE_CSV_FILE_PATH);
+            Assert.assertEquals(29, counter);
+        } catch (CensusAnalyserException e) {
+        }
+    }
 }
