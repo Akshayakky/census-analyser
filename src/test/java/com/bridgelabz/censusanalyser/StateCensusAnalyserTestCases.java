78bbcbba8f0401f4d38f12a71b1e523a74c229cc
akshayakky <akshaysmpt01@gmail.com>
2020-03-18 20:14:04 +0530

    ADDED : Loading Indian State Census CSV

--- src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTestCases.java
+++ src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTestCases.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.censusanalyser;
 
 public class StateCensusAnalyserTestCases {
+    private static final String SAMPLE_CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
+    private static final String SAMPLE_CSV_INCORRECT_FILE_PATH = "src/test/resources/StateCensusInvalidData.csv";
+    private static final String SAMPLE_CSV_INCORRECT_FILE_NAME = "src/test/resources/IncorrectName.csv";
+    private static final String SAMPLE_CSV_INCORRECT_FILE_TYPE = "src/test/resources/StateCensusData.pdf";
 }
